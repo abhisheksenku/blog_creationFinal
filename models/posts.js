3c1260fc6dbf27e1741a20eb9310a1708e7d8113
Abhishek Uthukotasriram <62733295+abhisheksenku@users.noreply.github.com>
2025-05-19 14:25:48 +0530

    Create models/posts.js

---
+++ models/posts.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utility/sql');
+
+const Posts = sequelize.define('postsTable', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = Posts;
